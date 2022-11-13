b143e2bd90795b9ff1b637141e1dcf03d5e0c7a4
NVPL1234 <nlam39784@gmail.com>
2022-11-13 11:09:08 +0700

    finish eureka

---
+++ Eureka-Client/src/main/java/com/example/eureka_client/GreetingController.java
@@ -0,0 +1,9 @@
+package com.example.eureka_client;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public interface GreetingController {
+	
+	@RequestMapping("/greeting")
+    String greeting();
+}
